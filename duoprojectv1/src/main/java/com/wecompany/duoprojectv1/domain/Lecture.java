09aee5a0103bf7323e962a2e8728cb4d9bc6c005
joonwoo595 <jonbeo312@gmail.com>
2025-04-12 19:23:18 +0900

    Lecture 수정 cosIns 문자열을 제거하고 insId 필드만 남김

--- duoprojectv1/src/main/java/com/wecompany/duoprojectv1/domain/Lecture.java
+++ duoprojectv1/src/main/java/com/wecompany/duoprojectv1/domain/Lecture.java
@@ -7,11 +7,10 @@ import java.time.LocalDate;
 public class Lecture {
     private Integer cosId;
     private String cosName;
-    private String cosIns;
     private int price;
     private int totalSecond;
     private LocalDate createDate;
     private int depId;
     private int maxCapacity;
+    private Integer insId; // 담당 교수의 FK
 }
-
